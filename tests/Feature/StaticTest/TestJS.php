0c32310750172b5495c694d4eee10af893064a03
pdeantihuman <pdeantihuman@gmail.com>
2018-06-02 20:12:14 +0800

    add app id

---
+++ tests/Feature/StaticTest/TestJS.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\StaticTest;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TestJS extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
