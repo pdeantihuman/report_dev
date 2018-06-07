4d606b617bb3713d11bd4dbad001f1e0acbbcdd1
pdeantihuman <pdeantihuman@gmail.com>
2018-06-07 19:05:58 +0800

    refactor emit wechat

--- app/Traits/IssuesEmitWechat.php
+++ app/Traits/IssuesEmitWechat.php
@@ -9,7 +9,38 @@
 namespace App\Traits;
 
 
+use App\Issue;
+use GuzzleHttp\Client;
+
 trait IssuesEmitWechat
 {
-
+    /**
+     * @param Issue $issue
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function emitWechat(Issue $issue) {
+        /**
+         * 组装数据
+         */
+        $data = [
+            'location' => $issue->alley.'教'.$issue->room.'室',
+            'time' => $issue->created_at,
+            'content' => substr($issue->description,0,10),
+            'url' => url('issues',['issue'=>$issue->id])
+        ];
+        /**
+         * 构建 url
+         */
+        $base_url = env('WXINTERFACE');
+        $destination =$base_url.'/?';
+        foreach($data as $key => $value){
+            $variable = $key.'='.$value.'&';
+            $destination = $destination.$variable;
+        }
+        /**
+         * 发送数据
+         */
+        $client = new Client();
+        $client->request('GET',$destination);
+    }
 }
