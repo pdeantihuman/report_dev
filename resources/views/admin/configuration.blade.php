576b20710f263720a0193e2ae1cebf7472ddbaa6
pdeantihuman <pdeantihuman@gmail.com>
2018-11-08 14:44:56 +0800

    build configuration blade template

---
+++ resources/views/admin/configuration.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.app')
+
+@section('title')
+    配置中心
+@endsection
+
+@section('brand')
+    配置中心
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <configuration-list></configuration-list>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/configuration.js') }}" defer></script>
+@endsection
